8817b7ace2d6ebf3a77d9d4e330c7aa31d281179
Alrafi Siddiquee Jim <alrafisiddiquee4@gmail.com>
2025-09-14 09:13:03 +0600

    Add files via upload

---
+++ Lec_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main(){
+
+    printf("hello world\n"); // this is a comment
+     printf("i am learning ")  ;
+    return 0;
+
+ }
+
+// ctrl + alt+ N = for running code
+// ctrl + L =   clear
+// clear + enter 
